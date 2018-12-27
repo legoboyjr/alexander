67e0e2b145d0e5756d939c285523dfdb6248c028
Alexander Thompson <legoboyjr@gmail.com>
2018-12-27 17:34:29 -0500

    Updates

---
+++ precache-manifest.b978a6d5a135fddf7f052b63951ebab7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9538e44c0148c433166",
+    "url": "/alexander/static/js/runtime~main.e9538e44.js"
+  },
+  {
+    "revision": "e61ad02b91e8b4163827",
+    "url": "/alexander/static/js/main.e61ad02b.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/alexander/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "e61ad02b91e8b4163827",
+    "url": "/alexander/static/css/main.05d68e32.chunk.css"
+  },
+  {
+    "revision": "f862d281f2a9b95651a47e3dd468d0fd",
+    "url": "/alexander/index.html"
+  }
+];
